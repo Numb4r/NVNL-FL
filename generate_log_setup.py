681481d40c2e6a8aacbe9b60d082c169c04c9d6f
yuri <yuri.d.d.faria@gmail.com>
2024-12-10 14:44:23 -0300

    modify script to create log

--- generate_log_setup.py
+++ generate_log_setup.py
@@ -1,5 +1,6 @@
 import os
 import re
+import yaml
 from datetime import datetime
 
 # Função para criar a pasta logs/data-horario
@@ -10,35 +11,117 @@ def create_log_directory():
     return directory
 
 # Função para extrair o valor da variável NOT_ENCRYPT_LAYERS como texto do arquivo client.py
-def extract_not_encrypt_layers(filepath):
-    with open("./client/client.py", 'r') as file:
+def type_technique(data):
+    
+    # technique=''
+    
+    if 'services' in data and 'server' in data['services']:
+        server_type = None
+        # Verifica as variáveis de ambiente para o servidor
+        for env_var in data['services']['server'].get('environment', []):
+            if env_var.startswith('TYPE='):
+                server_type = env_var.split('=')[1]
+                return f'TYPE={server_type}\n'
+        #         break
+        # if server_type:
+        #     types['server'] = server_type
+    # Obtém os tipos dos clientes
+    # if 'services' in data:
+    #     client_types = {}
+    #     for service_name, service_data in data['services'].items():
+    #         if service_name.startswith('client'):
+    #             client_type = None
+    #             # Verifica as variáveis de ambiente para o cliente
+    #             for env_var in service_data.get('environment', []):
+    #                 if env_var.startswith('TYPE='):
+    #                     client_type = env_var.split('=')[1]
+    #                     break
+    #             if client_type:
+    #                 client_types[service_name] = client_type
+    #     if client_types:
+    #         types['clients'] = client_types
+        # string_return = f"server = {types['server']}\n"
+        # string_return+="".join([f"{item[0]} = {item[1]}\n" for item in types['clients'].items()])
+        
+        # return string_return
+
+
+def fedphe_block_number():
+    with open("./teste-mnist-2.yaml",'r') as file:
+        data = yaml.safe_load(file)
+    if 'services' in data and 'server' in data['services']:
+        for env_var in data['services']['server'].get('environment', []):
+            if env_var.startswith('TYPE='):
+                server_type = env_var.split('=')[1]
+                if server_type != 'FEDPHE':
+                    
+                    return ''
+                break
+    with open("./client/FEDPHE.py", 'r') as file:
         content = file.read()
     
-    # Usando regex para capturar o valor da variável NOT_ENCRYPT_LAYERS
-    match = re.search(r'self\.NOT_ENCRYPTED_LAYERS\s*=\s*(\d+)', content)
+    
+    match = re.search(r'self\.QNT_BLOCOS\s*=\s*(\d+)', content)
+    print(match)
     if match:
-        return match.group(1)
+        return f'QNT_BLOCOS = {match.group(1)}\n'
     else:
         return None
-
 # Função para salvar o valor extraído em um arquivo txt
-def save_to_log_file(directory, value):
-    if value is not None:
-        log_file_path = os.path.join(directory, "info.txt")
-        with open(log_file_path, 'w') as file:
-            file.write(f"NOT_ENCRYPTED_LAYERS = {value}")
-        print(f"Valor salvo em {log_file_path}")
-    else:
-        print("Variável NOT_ENCRYPT_LAYERS não encontrada.")
+def save_to_log_file(directory, list_value):
+    log_file_path = os.path.join(directory, "info.txt")
+    with open(log_file_path, 'w') as file:
+        for value in list_value:
+            if value is not None:
+                    file.write(f"{value}")
+            print(f"Valor salvo em {log_file_path}")
 
+
+    
+         
+        #         break
+        # if server_type:
+        #     types['server'] = server_type
+def extract_info_yaml():
+    with open("./teste-mnist.yaml",'r') as file:
+            data = yaml.safe_load(file)
+    string_r = ''
+    infos_server = ['NCLIENTS',"NUM_ROUNDS","TYPE","DATASET","DIRICHLET_ALPHA"]
+    info_client = ['NIID','DIRICHLET_ALPHA']
+    if 'services' in data and 'server' in data['services']:
+        # Verifica as variáveis de ambiente para o servidor
+        for info in infos_server:
+            for env_var in data['services']['server'].get('environment', []):
+                
+                
+                if env_var.startswith(f'{info}='):
+                    str_extract = env_var.split('=')[1]
+                    string_r+=f'{info}={str_extract}\n'
+    else:
+        string_r+=f'Não foi possivel extrair as informações sobre o servidor\n'
+    if 'services' in data and 'client-0' in data['services']:
+        for info in info_client:
+            for env_var in data['services']['client-0'].get('environment',[]):
+                 if env_var.startswith(f'{info}='):
+                    str_extract = env_var.split('=')[1]
+                    string_r+=f'{info}={str_extract}\n'
+    else:
+        string_r+=f'Não foi possivel extrair as informações sobre os clientes\n'
+    return string_r
 # Caminho do arquivo client.py
 client_file_path = "client.py"
 
 # Criando o diretório de logs
 log_directory = create_log_directory()
 
-# Extraindo o valor de NOT_ENCRYPT_LAYERS
-not_encrypt_value = extract_not_encrypt_layers(client_file_path)
+
+
+
 
 # Salvando o valor em um arquivo txt
-save_to_log_file(log_directory, not_encrypt_value)
+save_to_log_file(log_directory, [
+    extract_info_yaml(),
+    
+    fedphe_block_number(),
+
+    ])
