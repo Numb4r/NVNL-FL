195148118e3a00b7830e5d0a010101bd97ef5771
yuri <yuri.d.d.faria@gmail.com>
2024-12-10 14:43:59 -0300

    modify script to create dockcompose

--- create_dockercompose.py
+++ create_dockercompose.py
@@ -3,12 +3,13 @@ from optparse import OptionParser
 import random
 
 
-def add_server_info(clients, rounds, solution, dataset, frac_fit, alpha):
+def add_server_info(clients, rounds, solution, dataset, frac_fit, alpha,technique):
     server_str = f"  server:\n\
     image: 'allanmsouza/flhe:server'\n\
     container_name: fl_server\n\
     environment:\n\
       - SERVER_IP=0.0.0.0:9999\n\
+      - TYPE={technique}\n\
       - NCLIENTS={clients}\n\
       - NUM_ROUNDS={rounds}\n\
       - SOLUTION_NAME={solution}\n\
@@ -30,11 +31,12 @@ def add_server_info(clients, rounds, solution, dataset, frac_fit, alpha):
 
     return server_str
 
-def add_client_info(cid, nclients, solution, dataset, niid, alpha):
+def add_client_info(cid, nclients, solution, dataset, niid, alpha,technique):
     client_str = f"  client-{cid}:\n\
     image: 'allanmsouza/flhe:client'\n\
     environment:\n\
       - SERVER_IP=fl_server:9999\n\
+      - TYPE={technique}\n\
       - CID={cid}\n\
       - SOLUTION_NAME={solution}\n\
       - DATASET={dataset}\n\
@@ -98,9 +100,10 @@ def add_prometheus_grafana_cadvisor():
 def main():
 
     parser = OptionParser()
-    # python create_dockercompose.py -c 2 -s teste -d mnist -r 10 --dirichilet 0.5 --frac-fit 1 --niid False
+    # python create_dockercompose.py -c 2 -s teste -d mnist -r 10 --dirichilet 0.5 --frac-fit 1 --niid False -t YPHE
     parser.add_option("-c", "--clients",            dest="clients",  default=0)
     parser.add_option("-s", "--sol",                dest="solution", default=0)
+    parser.add_option("-t","--technique",           dest="technique",default="YPHE")
     parser.add_option("-d", "--dataset",            dest="dataset",  default='MNIST')
     parser.add_option("-r", "--rounds",             dest="rounds",   default=100)
     parser.add_option("",   "--dirichilet",         dest="dirichilet",   default=1)
@@ -110,22 +113,22 @@ def main():
 
     (opt, args) = parser.parse_args()
 
-    with open(f'{opt.solution}-{opt.dataset}-{opt.clients}.yaml', 'a') as dockercompose_file:
+    with open(f'{opt.solution}-{opt.dataset}.yaml', 'w') as dockercompose_file:
         header = f"version: '3'\nservices:\n\n"
 
         dockercompose_file.write(header)
                                     
-        server_str = add_server_info(opt.clients, opt.rounds, opt.solution, opt.dataset, opt.frac_fit, opt.dirichilet)
+        server_str = add_server_info(opt.clients, opt.rounds, opt.solution, opt.dataset, opt.frac_fit, opt.dirichilet,opt.technique)
 
         dockercompose_file.write(server_str)
 
         for cid in range(int(opt.clients)):
-            client_str = add_client_info(cid, opt.clients, opt.solution, opt.dataset, opt.niid, opt.dirichilet)    
+            client_str = add_client_info(cid, opt.clients, opt.solution, opt.dataset, opt.niid, opt.dirichilet,opt.technique)    
 
             dockercompose_file.write(client_str)
             
-        prometheus_str = add_prometheus_grafana_cadvisor()
-        dockercompose_file.write(prometheus_str)
+        # prometheus_str = add_prometheus_grafana_cadvisor()
+        # dockercompose_file.write(prometheus_str)
 
 if __name__ == '__main__':
 	main()
