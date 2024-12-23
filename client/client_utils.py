6537a9f65204e13118673d0f9821209155d05883
yuri <yuri.d.d.faria@gmail.com>
2024-12-23 15:01:27 -0300

    implement another methods for mask generation

--- client/client_utils.py
+++ client/client_utils.py
@@ -2,7 +2,7 @@ import sys
 import tenseal as ts
 import numpy as np
 from models import flat_parameters
-
+import math
 def get_size(data):
     total_size = 0
     
@@ -63,6 +63,44 @@ def get_topk_mask(packs, percentage):
             res_mask[i] = 1
             
     return res_mask
+def get_slice_window_mask(round,packs,size_window=0.1,stride=3,percentage=False):
+    if percentage:
+        size_window = math.ceil(len(packs) * size_window)
+        stride = math.ceil(size_window*stride)
+    
+    array_z = np.zeros(len(packs))
+    start = round*stride
+    end = round*stride+size_window
+    for i in range(start, end):
+        array_z[i % len(array_z)] = 1
+    return array_z
+
+def get_robin_round_mask(round,packs,size_window,percentage=False):
+    if percentage:
+        size_window = math.ceil(len(packs) * size_window)
+    return get_slice_window_mask(round,packs,size_window,size_window)
+def get_pondering_random_mask(packs,k,weights_packs,penalty=0.5,reward=0.1,percentage=False):
+
+    n = len(packs)
+    if percentage:
+        k = math.ceil(k*n)
+    probabilities = weights_packs / weights_packs.sum()
+    mask = np.zeros(n)
+
+    
+    chosen = np.random.choice(range(n), size=k, replace=False, p=probabilities)
+    
+    for i in range(n):
+        if i in chosen:
+            weights_packs[i] *= penalty  
+            mask[i] = 1
+        else:
+            weights_packs[i] += reward  # Aumenta o peso dos não escolhidos
+
+    
+    weights_packs = np.maximum(weights_packs, 0.01)
+    return mask
+
 
 
 def remove_padding(self, packed_parameters):
